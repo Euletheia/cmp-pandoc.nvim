6d419ab9ac53e86491892880599c592012ded3ed
Euletheia <euletheia@tuta.io>
2024-09-24 11:40:52 +0200

    Multiple .json test

--- lua/cmp_pandoc/parse.lua
+++ lua/cmp_pandoc/parse.lua
@@ -6,28 +6,28 @@ local M = {}
 
 local function combine_names(names, suffix)
   local full_names = {}
-  for _,name in ipairs(names) do
-    local full_name = name.literal or name.family or ''
-    local non_drop = name['non-dropping-particle'] or ''
+  for _, name in ipairs(names) do
+    local full_name = name.literal or name.family or ""
+    local non_drop = name["non-dropping-particle"] or ""
     full_name = non_drop .. full_name
-    local given = name.given or ''
-    local drop = name['dropping-particle'] or ''
-    if drop ~= '' then
-      given = given .. ' ' .. drop
+    local given = name.given or ""
+    local drop = name["dropping-particle"] or ""
+    if drop ~= "" then
+      given = given .. " " .. drop
     end
-    if given ~= '' then
-      full_name = given .. ' ' .. full_name
+    if given ~= "" then
+      full_name = given .. " " .. full_name
     end
-    if full_name ~= '' then
+    if full_name ~= "" then
       table.insert(full_names, full_name)
     end
   end
-  local full_names_str = table.concat(full_names, ' and ')
+  local full_names_str = table.concat(full_names, " and ")
   if suffix then
     if #names > 1 then
-      full_names_str = full_names_str .. ' (' .. suffix .. 's)'
+      full_names_str = full_names_str .. " (" .. suffix .. "s)"
     else
-      full_names_str = full_names_str .. ' (' .. suffix .. ')'
+      full_names_str = full_names_str .. " (" .. suffix .. ")"
     end
   end
 
@@ -36,24 +36,23 @@ end
 
 -- Parse the .json file
 local function parse_json_bib(filename)
-  local file = io.open(filename, 'rb')
+  local file = io.open(filename, "rb")
   local bibentries = nil
   if file ~= nil then
-    bibentries = file:read('*all')
+    bibentries = file:read("*all")
     file:close()
   end
   local data = vim.json.decode(bibentries)
-  return vim.tbl_map(
-    function(item)
-    local id = item.id or ''
+  return vim.tbl_map(function(item)
+    local id = item.id or ""
     local doc = {}
-    local type = item.type or ''
-    type = type:gsub("^%l", string.upper):gsub('-journal','')
-    table.insert(doc, '# ' .. type)
+    local type = item.type or ""
+    type = type:gsub("^%l", string.upper):gsub("-journal", "")
+    table.insert(doc, "# " .. type)
 
-    local title = item['title-short'] or item.title or ''
-    title = title:gsub('[{}]+','')
-    table.insert(doc, '*' .. title .. '*')
+    local title = item["title-short"] or item.title or ""
+    title = title:gsub("[{}]+", "")
+    table.insert(doc, "*" .. title .. "*")
 
     local authors = item.author or nil
     if authors then
@@ -63,47 +62,65 @@ local function parse_json_bib(filename)
 
     local editors = item.editor or nil
     if editors then
-      editors = combine_names(editors, 'ed')
+      editors = combine_names(editors, "ed")
       table.insert(doc, editors)
     end
 
     local translators = item.translator or nil
     if translators then
       translators = combine_names(translators, nil)
-      table.insert(doc, translators .. ' (trans)')
+      table.insert(doc, translators .. " (trans)")
     end
 
     local date = item.issued or nil
     if date then
       date = date.literal or nil
       if date then
-         table.insert(doc, date)
+        table.insert(doc, date)
       else
-        date = item.issued['date-parts'] or nil
+        date = item.issued["date-parts"] or nil
         if date then
           date = date[1] or nil
         end
-        if date then
-          date = date[1] or nil
-        end
+        -- NOTE : Is that an duplicate mistake ?
+        -- if date then
+        --   date = date[1] or nil
+        -- end
         if date then
           table.insert(doc, date)
         end
       end
     end
 
-    local doc_str = table.concat(doc, '\n')
+    local original_date = item["original-date"] or nil
+    if original_date then
+      original_date = original_date.literal or nil
+      if original_date then
+        table.insert(doc, original_date)
+      else
+        original_date = item["original-date"]["date-parts"] or nil
+        if original_date then
+          original_date = original_date[1] or nil
+        end
+        if date then
+          table.insert(doc, original_date)
+        end
+      end
+    end
+
+    -- TODO : Add original_publisher / original_publisher_place
+
+    local doc_str = table.concat(doc, "\n")
     return {
-      label = '@' .. id,
+      label = "@" .. id,
       kind = cmp.lsp.CompletionItemKind.Reference,
       documentation = {
         kind = cmp.lsp.MarkupKind.Markdown,
         value = doc_str,
-      }
+      },
     }
   end, data)
 end
-
 
 local crossreferences = function(line, opts)
   if string.match(line, utils.crossref_patterns.equation) and string.match(line, "^%$%$(.*)%$%$") then
@@ -159,21 +176,29 @@ M.references = function(bufnr, opts)
   end, valid_lines)
 end
 
+-- NOTE : The objective here is to allow multiple csl to be parsed into the all_entrys cmp source
 M.init = function(self, callback, bufnr)
   local opts = self and self.opts or require("cmp_pandoc.config")
 
-  local bib = opts.bibliography.path
-  if bib == nil then
-    return callback()
-  end
-  local bib_type = string.match(bib,"[^.]+$")
-
-  if bib_type ~= 'json' then
+  local bibfield = opts.bibliography.path
+  if bibfield == nil then
     return callback()
   end
 
-  local bib_items = nil
-  bib_items = parse_json_bib(bib)
+  local bibs = {}
+
+  for i = 1, #bibfield do
+    local bib_type = string.match(bibfield[i], "[^.]+$")
+    if bib_type == "json" then
+      vim.list_extend(bibs, bibfield[i])
+    end
+  end
+
+  local bib_items = {}
+  for i = 1, #bibs do
+    local bib_item = parse_json_bib(bibs[i])
+    vim.list_extend(bib_items, bib_item)
+  end
 
   local reference_items = M.references(bufnr, opts.crossref)
 
@@ -192,7 +217,6 @@ M.init = function(self, callback, bufnr)
   end
 
   return callback(all_entrys)
-
 end
 
 return M
