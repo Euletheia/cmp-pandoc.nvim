1e55e1fe1b06bd1831c4c34e1ecf91ceca8c160c
Euletheia <euletheia@tuta.io>
2024-09-26 14:18:30 +0200

    Testing

--- lua/cmp_pandoc/parse.lua
+++ lua/cmp_pandoc/parse.lua
@@ -187,15 +187,17 @@ M.init = function(self, callback, bufnr)
 
   local bibs = {}
 
-  -- TODO : Extend $HOME variable if found in path
   for i = 1, #bibfield do
     local bib_type = string.match(bibfield[i], "[^.]+$")
     if bib_type == "json" then
       local bibpath = vim.fn.expand(bibfield[i])
       print(bibpath)
-      vim.list_extend(bibs, bibpath)
+      -- vim.fn.table_insert(bibs, bibpath)
+      table.insert(bibs, bibpath)
+      -- vim.list_extend(bibs, { bibpath })
     end
   end
+  print(bibs)
 
   local bib_items = {}
   for i = 1, #bibs do
